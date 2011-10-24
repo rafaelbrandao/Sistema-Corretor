cf6f089b9a4264c1b044bf229953ee18c76d029a
Rafael Brandão <rbl@cin.ufpe.br>
2011-10-23 22:35:47 -0200

    Fix last view to show list name instead of a invalid fixed value.

--- application/views/v_admin_add_problem.php
+++ application/views/v_admin_add_problem.php
@@ -10,7 +10,7 @@ if (!isset($problem_num)) $problem_num = '';
 	<li>Criar Questão</li>
 </ul>
 <pre>
-<h1><strong>Lista 2</strong> - Criar Questão</h1>
+<h1><strong>Lista '<?=$listprefix?>'</strong> - Criar Questão</h1>
 Para criar uma questão você só precisa especificar seu número. Uma vez com a questão criada, você pode editá-la da maneira que quiser. A primeira questão da lista deve ter o número 1, e os números das demais devem seguir a sequência a partir de 1.
 
 <?=form_open(base_url('/index.php/monitor/add_problem/'.$list_id))?>
