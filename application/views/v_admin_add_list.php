b71562552fd2558635e9a6a901f0fc52902569a5
Rafael Brandão <rbl@cin.ufpe.br>
2011-11-27 23:42:14 -0300

    Review of create list and editing list views to align forms and text.

--- application/views/v_admin_add_list.php
+++ application/views/v_admin_add_list.php
@@ -12,13 +12,14 @@ if (!isset($timeend)) $timeend = '';
 <h1>Criar Lista</h1>
 Para o campo <strong>Lista (prefixo)</strong>, sugerimos que use no formato <strong>LX</strong> onde X é o número da lista. Esse prefixo será depois usado para o nome dos arquivos de entrada e de saída, por exemplo: <strong>LXQY</strong>, caso o prefixo seja no formato sugerido anteriormente. Cada lista diferente terá um prefixo diferente.
 
-Para os campos <strong>Inicio</strong> e <strong>Prazo</strong>, coloque a data no formato "<strong>DD/MM HH:mm</strong>", por exemplo: <strong>13/09 13:50</strong>. Se não quiser especificá-las agora, deixe-as em branco. Uma lista só estará disponível para o público quando essas duas datas estiverem bem definidas (e depois da data de lançamento da lista).
+Para os campos <strong>Inicio</strong> e <strong>Término</strong>, coloque a data no formato "<strong>DD/MM HH:mm</strong>", por exemplo: <strong>13/09 13:50</strong>. Se não quiser especificá-las agora, deixe-as em branco. Uma lista só estará disponível para o público quando essas duas datas estiverem bem definidas (e obviamente se o horario atual estiver dentro do intervalo definido).
 
-<?=form_open(base_url('/index.php/monitor/add_list'))?>
-Lista (prefixo): <input name="listprefix" value="<?=$listprefix?>" type="text" style="width: 50px;" />
-Inicio: <input name="timebegin" value="<?=$timebegin?>" type="text" style="width: 100px;" />
-Prazo: <input name="timeend" value="<?=$timeend?>" type="text" style="width: 100px;" />
-<input type="submit" value="Criar" />
-</form>
+<?=form_open(base_url('/index.php/monitor/add_list'))?><div id="register_block">
+Lista (prefixo): <input name="listprefix" value="<?=$listprefix?>" type="text" style="position: absolute; right: 0px; width: 150px;" />
+Inicio: <input name="timebegin" value="<?=$timebegin?>" type="text" style="position: absolute; right: 0px; width: 150px;" />
+Término: <input name="timeend" value="<?=$timeend?>" type="text" style="position: absolute; right: 0px; width: 150px;" />
+
+<input type="submit" value="Criar" style="position: absolute; right: 0px; width: 50px;" />
+</form></div>
 
 </pre>
--- application/views/v_admin_edit_list.php
+++ application/views/v_admin_edit_list.php
@@ -14,24 +14,25 @@ if (!isset($rev_timeend)) $rev_timeend='';
 </ul>
 <pre>
 <h1><strong>Lista '<?=$listprefix?>'</strong> - Editar Lista</h1>
-Para os campos <strong>Inicio</strong> e <strong>Prazo</strong>, coloque a data no formato "<strong>DD/MM HH:mm</strong>", por exemplo: <strong>13/09 13:50</strong>. Da mesma forma para os campos <strong>Revisão (início)</strong> e <strong>Revisão (prazo)</strong>. Se não quiser modificá-los agora, basta manter os valores atuais inalterados.
+Para os campos <strong>Inicio</strong> e <strong>Término</strong>, coloque a data no formato "<strong>DD/MM HH:mm</strong>", por exemplo: <strong>13/09 13:50</strong>. Da mesma forma para os campos <strong>Revisão (início)</strong> e <strong>Revisão (término)</strong>. Se não quiser modificá-los agora, basta manter os valores atuais inalterados.
 
-Uma lista só estará disponível para o público quando das datas de inicio e prazo estiverem bem definidas (e depois da data de lançamento da lista). Da mesma forma para as revisões dessa lista, que só estará disponível para os alunos quando as duas datas forem definidas.
+Uma lista só estará disponível para o público quando das datas de inicio e de término estiverem bem definidas, o horário atual estiver neste intervalo e estiver no estado "<strong>Andamento</strong>" ou adiante. Da mesma forma para as revisões dessa lista: só estará disponível para os alunos quando as duas datas forem definidas e estiver no estado "<strong>Revisão</strong>".
 
-<?=form_open(base_url('/index.php/monitor/edit_list/'.$list_id))?>
-Lista (prefixo): <input name='listprefix' value="<?=$listprefix?>" type="text" style="width: 100px;" />
-Estado: <select name="liststate" style="width: 100px;">
+<div id="register_block"><?=form_open(base_url('/index.php/monitor/edit_list/'.$list_id))?>
+Lista (prefixo): <input name='listprefix' value="<?=$listprefix?>" type="text" style="position: absolute; right: 0px; width: 150px;" />
+Estado: <select name="liststate" style="position: absolute; right: 0px; width: 150px;">
 <option value="preparacao" <?=($liststate == 'preparacao' ? 'selected' : '')?>>Preparação</option>
 <option value="andamento" <?=($liststate == 'andamento' ? 'selected' : '')?>>Andamento</option>
 <option value="correcao" <?=($liststate == 'correcao' ? 'selected' : '')?>>Correção</option>
 <option value="revisao" <?=($liststate == 'revisao' ? 'selected' : '')?>>Revisão</option>
 <option value="finalizada" <?=($liststate == 'finalizada' ? 'selected' : '')?>>Finalizada</option>
 </select>
-Inicio: <input name='timebegin' value="<?=$timebegin?>" type="text" style="width: 100px;" />
-Prazo: <input name='timeend' value="<?=$timeend?>" type="text" style="width: 100px;" />
-Revisão (inicio): <input name='rev_timebegin' value="<?=$rev_timebegin?>" type="text" style="width: 100px;" />
-Revisão (prazo): <input name='rev_timeend' value="<?=$rev_timeend?>" type="text" style="width: 100px;" />
 
-<input type="submit" value="Salvar alterações" /></form>
+Inicio: <input name='timebegin' value="<?=$timebegin?>" type="text" style="position: absolute; right: 0px; width: 150px;" />
+Término: <input name='timeend' value="<?=$timeend?>" type="text" style="position: absolute; right: 0px; width: 150px;" />
+Revisão (inicio): <input name='rev_timebegin' value="<?=$rev_timebegin?>" type="text" style="position: absolute; right: 0px; width: 150px;" />
+Revisão (término): <input name='rev_timeend' value="<?=$rev_timeend?>" type="text" style="position: absolute; right: 0px; width: 150px;" />
+
+<input type="submit" value="Salvar alterações" style="position: absolute; right: 0px; width: 150px;"/></form></div>
 
 </pre>
